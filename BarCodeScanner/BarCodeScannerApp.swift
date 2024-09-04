da38ca754cc6baa50f788b18fcdd11be29ca8738
ZakTan <Ztanksley@luc.edu>
2024-09-04 14:28:49 -0700

    Initial Commit

---
+++ BarCodeScanner/BarCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeScannerApp.swift
+//  BarCodeScanner
+//
+//  Created by Zakee Tanksley on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
---
+++ BarCodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,54 @@
+//
+//  BarcodeScannerView.swift
+//  BarCodeScanner
+//
+//  Created by Zakee Tanksley on 9/4/24.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        NavigationView{
+            VStack{
+                Spacer()
+                
+                Rectangle().frame(maxWidth: .infinity,maxHeight: 200).border(Color.black)
+                Spacer()
+                
+                Button(action: {
+                    // Action to perform when the button is tapped
+                }) {
+                    HStack {
+                        Image(systemName: "barcode.viewfinder")
+                        Text("Scan Barcode")
+                    }
+                    .font(.headline)
+                    .padding(20)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+                }
+                
+                Text("Not Scanned").font(.title).foregroundColor(Color.red).padding()
+                Spacer()
+            }
+            .navigationTitle("Barcode Scanner")
+        }
+    }
+}
+struct scanButton: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "barcode.viewfinder")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Scan Barcode")
+        }
+    }
+    
+}
+
+#Preview {
+    BarcodeScannerView()
+}
